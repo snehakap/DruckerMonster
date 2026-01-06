4518ad08227abdc5acf65c11124cfa19b12df8c2
Sneha Kapoor <sneha.kapoor@somaiya.edu>
2026-01-06 14:23:21 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+
+  # Druckermonster Website Design
+
+  This is a code bundle for Druckermonster Website Design. The original project is available at https://www.figma.com/design/80Mkjk7VxTXKs9xEiA8dPI/Druckermonster-Website-Design.
+
+  ## Running the code
+
+  Run `npm i` to install the dependencies.
+
+  Run `npm run dev` to start the development server.
+  
---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    host: true,
+    allowedHosts: ['.loca.lt'],
+  },
+})
