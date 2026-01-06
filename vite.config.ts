89f0ae82160fbb060694cd4de650e177c5d5209c
Sneha Kapoor <sneha.kapoor@somaiya.edu>
2026-01-06 14:42:33 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,8 +13,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  server: {
-    host: true,
-    allowedHosts: ['.loca.lt'],
+  build: {
+    outDir: 'dist',
   },
 })
